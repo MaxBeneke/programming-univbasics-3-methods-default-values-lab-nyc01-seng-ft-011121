713e8e885491043c947def30b2be60b1ebbafc9c
Max Beneke <max.beneke@gmail.com>
2020-12-07 14:57:13 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,4 @@
 def meal_choice(protein='tofu', veg1, veg2)
   puts "What a nutritious meal!"
-  puts "A play of #{protein} with #{veg1} and #{veg2}."
+  p "A play of #{protein} with #{veg1} and #{veg2}."
 end
